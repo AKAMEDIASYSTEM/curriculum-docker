47b9e994f17fff844e2eb996ff83da15740cec6c
AKAMEDIASYSTEM <nfeehan@gmail.com>
2015-04-08 19:05:02 -0400

    oh my, i am going back to mongo

--- server/handlers/ApiHandler.py
+++ server/handlers/ApiHandler.py
@@ -15,15 +15,20 @@ class ApiHandler(BaseHandler):
     def get(self):
         if self.isAuth():
             n = self.get_argument('n',3) # three hours, should be global EXPIRE_IN from worker.py
+            ty = self.get_argument('type','any')
             earliest = datetime.datetime.utcnow() - datetime.timedelta(days=1)
             groupID = self.get_argument('groupID') # these have to be present, isAuth would have failed otherwise
             db = self.settings['db']
             print 'hit the ApiHandler endpoint with n=', n
             keywords = []
             found = 0
-            r = db.keywords.find({'latest':{'$gte':earliest},'groupID':groupID},{'keyword':1,'_id':0})
+            if ty=='any':
+                r = db.keywords.find({'latest':{'$gte':earliest},'groupID':groupID},{'keyword':1,'_id':0})
+            else:
+                # insecure! we should have an enum or whitelist of types
+                r = db.keywords.find({'latest':{'$gte':earliest},'groupID':groupID, 'type':ty},{'keyword':1,'_id':0})
             results = [word['keyword'] for word in r]
-            while found < 4:
+            while found < int(n):
                 k = random.choice(results)
                 if k not in keywords:
                     keywords.append(k)
