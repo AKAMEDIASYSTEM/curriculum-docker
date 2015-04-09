54d86f92161843dcbd637be1c6625c67256d53ff
AKAMEDIASYSTEM <nfeehan@gmail.com>
2015-04-08 20:11:05 -0400

    tuning apihandler

--- server/handlers/ApiHandler.py
+++ server/handlers/ApiHandler.py
@@ -26,7 +26,7 @@ class ApiHandler(BaseHandler):
                 r = db.keywords.find({'latest':{'$gte':earliest},'groupID':groupID},{'keyword':1,'_id':0})
             else:
                 # insecure! we should have an enum or whitelist of types
-                r = db.keywords.find({'latest':{'$gte':earliest},'groupID':groupID, 'type':ty},{'keyword':1,'type':1,'_id':0})
+                r = db.keywords.find({'latest':{'$gte':earliest},'groupID':groupID, 'type':ty.upper()},{'keyword':1,'type':1,'_id':0})
                 # r = db.keywords.find({'latest':{'$gte':earliest},'groupID':groupID, 'type':ty}) # this was to test query
             results = [word['keyword'] for word in r]
             while found < int(n):
