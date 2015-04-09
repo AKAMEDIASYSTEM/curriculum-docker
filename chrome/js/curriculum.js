dd3d50161617fe5144629b2139827e80a95d9acc
AKAMEDIASYSTEM <nfeehan@gmail.com>
2015-04-08 21:18:06 -0400

    tuning apihandler

--- chrome/js/curriculum.js
+++ chrome/js/curriculum.js
@@ -1,7 +1,8 @@
 
 // var ec2 = "http://ec2-107-21-88-135.compute-1.amazonaws.com:8888";
-var ec2 = "http://curriculum.nytlabs.com";
-var ignores = ["curriculum.nytlabs.com","ec2-107-21-88-135.compute-1.amazonaws.com","localhost","192.168.","127.0."]; // do not semantically analyze URLs contining these strings
+// var ec2 = "http://curriculum.nytlabs.com";
+var ec2 = "http://ec2-52-0-128-225.compute-1.amazonaws.com"
+var ignores = ["curriculum.nytlabs.com","ec2-52-0-128-225.compute-1.amazonaws.com","localhost","192.168.","127.0."]; // do not semantically analyze URLs contining these strings
 var path = window.location.href;
 var isHttps = path.split(":")[0];
 var time = new Date().getTime();
@@ -10,21 +11,17 @@ var flagDiscard = false;
 
 
 if(window.location.protocol == "https:") {
-    // fully ignore
     console.log("It's https");
+    // do nothing
     } else {
         for(var i=0; i<ignores.length; i++){
-            
+            // check to see if it's an ignorable domain
             if(path.indexOf(ignores[i]) != -1){
                 flagDiscard = true;
             }
         }
         if(flagDiscard){
-            console.log("It's a page we should ignore!");
-                message.queryData = {
-                    "url" : window.location.href.toString(),
-                };
-                message.ec2 = ec2+"/submitAnon";
+            // do nothing
         } else {
             message.queryData = {
             "url" : path,
--- server/server.py
+++ server/server.py
@@ -10,10 +10,6 @@ from handlers.ApiHandler import ApiHandler
 from handlers.SubmitHandler import SubmitHandler
 import datetime
 
-
-
-# db = redis.StrictRedis(host='localhost', port=6379, db=1)
-# auth = redis.StrictRedis(host='localhost', port=6379, db=2)
 client = MongoClient(tz_aware=True)
 db = client.curriculum
 # settings = {'debug':True}
