77ea113c57fe5cb9348b4da4616334c6476e5463
akamediasystem <nfeehan@gmail.com>
2015-05-27 14:39:13 -0400

    chasing down weird bug in which after 2 or so fine days, akaworker fails by not being able to fetch url?

--- worker/worker.py
+++ worker/worker.py
@@ -3,7 +3,8 @@
 # AKA tuned-resonator curriculum-insular resolver/worker
 
 import beanstalkc
-from pattern.web import URL, plaintext, URLError, MIMETYPE_WEBPAGE, MIMETYPE_PLAINTEXT, HTTPError
+# from pattern.web import URL, plaintext, URLError, MIMETYPE_WEBPAGE, MIMETYPE_PLAINTEXT, HTTPError
+import pattern.web
 from pattern.en import parsetree
 from pymongo import MongoClient
 import datetime
@@ -21,51 +22,54 @@ while True:
     # delete job
     print 'starting worker outer loop'
     job = beanstalk.reserve()  # this is blocking, waits till there's something on the stalk
-    pay = job.body.split('|')
-    groupID = pay[0]
-    url = URL(pay[-1])
+    payload = job.body.split('|')
+    groupID = payload[0]
+    url = pattern.web.URL(payload[-1])
     print 'new url, we think', url
     timestamp = datetime.datetime.utcnow()
     try:
         s = url.download(cached=True)
-        print url.mimetype
-        if (url.mimetype in MIMETYPE_WEBPAGE) or (url.mimetype in MIMETYPE_PLAINTEXT):
-            s = plaintext(s)
-            '''
-            parsetree(string,
-                   tokenize = True,         # Split punctuation marks from words?
-                       tags = True,         # Parse part-of-speech tags? (NN, JJ, ...)
-                     chunks = True,         # Parse chunks? (NP, VP, PNP, ...)
-                  relations = False,        # Parse chunk relations? (-SBJ, -OBJ, ...)
-                    lemmata = False,        # Parse lemmata? (ate => eat)
-                   encoding = 'utf-8'       # Input string encoding.
-                     tagset = None)         # Penn Treebank II (default) or UNIVERSAL.
-            '''
-            parsed = parsetree(s, chunks=True)
-            for sentence in parsed:
-                # only noun phrases for now but let's pick some good other ones next week
-                # seeing ADJP, ADVP, PP and VP mostly tho NP are predominant
-                # gen = (the_chunk for the_chunk in sentence.chunks if the_chunk.type=='NP')
-                # for chunk in gen:
-                for chunk in sentence.chunks:
-                    d = db.keywords.update(
-                        {'keyword': chunk.string, 'type': chunk.type, 'groupID': groupID},
-                        {'$push': {'timestamp': timestamp, 'url': url.string}, '$set': {'latest': timestamp}},
-                        upsert=True
-                        )
-                    # print d
-        else:
-            'we failed the mimetype test again wtf'
-    except HTTPError, e:
-        # e = sys.exc_info()[0]
-        # print 'URLError on ', url
-        print url
-        print e
-        pass
-    except e:
-        print e
-        print url
-        print 'AKA unhandled exception that we will try to just destroy without halting and catching fire'
+    except Exception as e:
+        print 'the url.download failed on %s' % url
+        print 'error is ', e
+        s = None
+    if (url.mimetype in pattern.web.MIMETYPE_WEBPAGE) or (url.mimetype in pattern.web.MIMETYPE_PLAINTEXT):
+        s = pattern.web.plaintext(s)
+        '''
+        parsetree(string,
+               tokenize = True,         # Split punctuation marks from words?
+                   tags = True,         # Parse part-of-speech tags? (NN, JJ, ...)
+                 chunks = True,         # Parse chunks? (NP, VP, PNP, ...)
+              relations = False,        # Parse chunk relations? (-SBJ, -OBJ, ...)
+                lemmata = False,        # Parse lemmata? (ate => eat)
+               encoding = 'utf-8'       # Input string encoding.
+                 tagset = None)         # Penn Treebank II (default) or UNIVERSAL.
+        '''
+        parsed = parsetree(s, chunks=True)
+        for sentence in parsed:
+            # only noun phrases for now but let's pick some good other ones next week
+            # seeing ADJP, ADVP, PP and VP mostly tho NP are predominant
+            # gen = (the_chunk for the_chunk in sentence.chunks if the_chunk.type=='NP')
+            # for chunk in gen:
+            for chunk in sentence.chunks:
+                d = db.keywords.update(
+                    {'keyword': chunk.string, 'type': chunk.type, 'groupID': groupID},
+                    {'$push': {'timestamp': timestamp, 'url': url.string}, '$set': {'latest': timestamp}},
+                    upsert=True
+                    )
+                print d
+    else:
+        'we failed the mimetype test, we think mimetype is ', url.mimetype
+    # except HTTPError, e:
+    #     # e = sys.exc_info()[0]
+    #     # print 'URLError on ', url
+    #     print url
+    #     print e
+    #     pass
+    # except e:
+    #     print e
+    #     print url
+    #     print 'AKA unhandled exception that we will try to just destroy without halting and catching fire'
     # end of if(isThere < 2)
     job.delete()
     print 'job deleted, we think this was all successful - loop over'
